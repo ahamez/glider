f3e5f1357c11d833030be0bb881774c729a06dc9
Alexandre Hamez <alexandre.hamez@gmail.com>
2018-03-06 22:46:15 +0100

    Extract grid

--- src/glider/grid.rs
+++ src/glider/grid.rs
@@ -1,21 +1,137 @@
 /* --------------------------------------------------------------------------------------------- */
 
-pub type Grid = Vec<Vec<bool>>;
+// TODO. Make this a trait
+//  - random access
+//  - build
+//  - count_live_neighbours
+
+#[derive(Clone)]
+pub struct Grid {
+  data: Vec<Vec<bool>>,
+  nb_lines: usize,
+  nb_columns: usize,
+}
 
 /* --------------------------------------------------------------------------------------------- */
 
-pub fn make_empty(width: usize, height: usize) -> Vec<Vec<bool>> {
-  let mut grid = Vec::with_capacity(width + 2);
+impl Grid {
 
-  for _x in 0..width {
-    let mut line = Vec::with_capacity(height + 2);
-    for _y in 0..height {
-      line.push(false);
-    }
-    grid.push(line);
+  pub fn new(nb_lines: usize, nb_columns: usize) -> Self {
+    let mut line = Vec::new();
+    line.resize(nb_columns + 2, false);
+
+    let mut data = Vec::new();
+    data.resize(nb_lines + 2, line);
+
+    Grid{data, nb_columns, nb_lines}
   }
 
-  grid
+  pub fn new_from(data: Vec<Vec<bool>>) -> Self {
+    assert!(data.len() >= 1);
+    assert!(data[0].len() >= 1);
+    // TODO. Check size consistency
+
+    let mut g = Self::new(data.len() + 2, data[0].len() + 2);
+
+    for x in 0 .. data.len() {
+      for y in 0 .. data[0].len() {
+        g.set(x, y, data[x][y]);
+      }
+    }
+
+    g
+  }
+
+  pub fn at(&self, x: usize, y: usize) -> bool {
+    self.data[x+1][y+1]
+  }
+
+  pub fn set(&mut self, x: usize, y: usize, value: bool) -> &mut Self {
+    self.data[x+1][y+1] = value;
+    self
+  }
+
+  pub fn count_live_neighbours(&self, x: usize, y: usize) -> u8 {
+    debug_assert!(self.data.len() > 2);
+    debug_assert!(self.data[0].len() > 2);
+
+    let x = x + 1;
+    let y = y + 1;
+
+      self.data[x-1][y-1] as u8
+    + self.data[x-1][y  ] as u8
+    + self.data[x-1][y+1] as u8
+    + self.data[x  ][y-1] as u8
+    + self.data[x  ][y+1] as u8
+    + self.data[x+1][y-1] as u8
+    + self.data[x+1][y  ] as u8
+    + self.data[x+1][y+1] as u8
+  }
+
+  pub fn nb_lines(&self) -> usize {
+    self.nb_lines
+  }
+
+  pub fn nb_columns(&self) -> usize {
+    self.nb_columns
+  }
+
+  pub fn count_live_cells(&self) -> u64 {
+    self.data.iter()
+      .fold(0, |acc, ref col| acc + col.iter().fold(0, |acc, ref cell| acc + **cell as u64))
+  }
+}
+
+/* --------------------------------------------------------------------------------------------- */
+/* --------------------------------------------------------------------------------------------- */
+
+#[test]
+fn test_count_live_neighbours() {
+  {
+    // 1x1 universe
+    let g = Grid::new_from(vec![
+      vec![false],
+    ]);
+
+    assert_eq!(g.count_live_neighbours(0, 0), 0);
+  }
+  {
+    // 1x1 universe
+    let g = Grid::new_from(vec![
+      vec![true],
+    ]);
+
+    assert_eq!(g.count_live_neighbours(0, 0), 0);
+  }
+  {
+    // 2x2 universe
+    let g = Grid::new_from(vec![
+        //   0       1
+      vec![true , false], // 0
+      vec![false, true ], // 1
+    ]);
+
+    assert_eq!(g.count_live_neighbours(0, 0), 1);
+    assert_eq!(g.count_live_neighbours(0, 1), 2);
+    assert_eq!(g.count_live_neighbours(1, 0), 2);
+    assert_eq!(g.count_live_neighbours(1, 1), 1);
+  }
+  {
+    // 3x3 universe
+    let g = Grid::new_from(vec![
+        //   0      1      2
+      vec![true , false, true ], // 0
+      vec![false, true , false], // 1
+      vec![false, false, false], // 2
+    ]);
+
+    assert_eq!(g.count_live_neighbours(0, 0), 1);
+    assert_eq!(g.count_live_neighbours(0, 1), 3);
+    assert_eq!(g.count_live_neighbours(0, 2), 1);
+    assert_eq!(g.count_live_neighbours(1, 0), 2);
+    assert_eq!(g.count_live_neighbours(1, 1), 2);
+    assert_eq!(g.count_live_neighbours(1, 2), 2);
+  }
 }
 
 /* --------------------------------------------------------------------------------------------- */
--- src/glider/universe.rs
+++ src/glider/universe.rs
@@ -1,45 +1,70 @@
-use super::grid::{make_empty, Grid};
+/* --------------------------------------------------------------------------------------------- */
+
+use super::grid::Grid;
 
 /* --------------------------------------------------------------------------------------------- */
 
 pub struct Universe {
   grid: Grid,
-  width: usize,
-  height: usize,
+  generation: u64,
+  live_cells: u64,
 }
 
 /* --------------------------------------------------------------------------------------------- */
 
 impl Universe {
   
-  pub fn new(width: usize, height: usize) -> Self {
+  pub fn new(columns: usize, lines: usize) -> Self {
     Universe{
-      grid: make_empty(width, height),
-      width,
-      height,
+      grid: Grid::new(columns, lines),
+      generation: 0,
+      live_cells: 0,
+    }
+  }
+
+  pub fn new_from(grid: Grid) -> Self {
+    let live_cells = grid.count_live_cells();
+
+    Universe{
+      grid,
+      generation: 0,
+      live_cells,
     }
   }
 
   pub fn tick(&self) -> Self {
     let mut next_grid = self.grid.clone();
+    let mut live_cells = 0;
 
-    for x in 1 .. self.width - 1 {
-      for y in 1 .. self.height - 1 {
-        next_grid[x][y] = tick_cell(&self.grid, x, y);
+    for x in 0 .. self.grid.nb_lines() {
+      for y in 0 .. self.grid.nb_columns() {
+        if tick_cell(&self.grid, x, y) {
+          live_cells += 1;
+          next_grid.set(x, y, true);
+        }
       }
     }
     
     Universe {
       grid: next_grid,
-      .. *self
+      generation: self.generation + 1,
+      live_cells
     }
+  }
+
+  pub fn generation(&self) -> u64 {
+    self.generation
+  }
+
+  pub fn live_cells(&self) -> u64 {
+    self.live_cells
   }
 }
 
 /* --------------------------------------------------------------------------------------------- */
 
 fn tick_cell(grid: &Grid, x: usize, y: usize) -> bool {
-  match (grid[x][y], count_live_neighbours(grid, x, y)) {
+  match (grid.at(x, y), grid.count_live_neighbours(x, y)) {
     (true , 2 ... 3) => true,
     (false, 3      ) => true,
     (_    , _      ) => false,
@@ -47,105 +72,38 @@ fn tick_cell(grid: &Grid, x: usize, y: usize) -> bool {
 }
 
 /* --------------------------------------------------------------------------------------------- */
-
-fn count_live_neighbours(grid: &Grid, x: usize, y: usize) -> u8 {
-  debug_assert!(grid.len() > 2);
-  debug_assert!(grid[0].len() > 2);
-
-    grid[x-1][y-1] as u8
-  + grid[x-1][y  ] as u8
-  + grid[x-1][y+1] as u8
-  + grid[x  ][y-1] as u8
-  + grid[x  ][y+1] as u8
-  + grid[x+1][y-1] as u8
-  + grid[x+1][y  ] as u8
-  + grid[x+1][y+1] as u8
-}
-
-/* --------------------------------------------------------------------------------------------- */
 /* --------------------------------------------------------------------------------------------- */
 
 #[test]
-fn test_count_live_neighbours() {
-  {
-    // 1x1 universe
-    let g = vec![
-      vec![false, false, false],
-      vec![false, false, false],
-      vec![false, false, false],
-    ];
-
-    assert_eq!(count_live_neighbours(&g, 1, 1), 0);
-  }
-  {
-    // 1x1 universe
-    let g = vec![
-      vec![false, false, false],
-      vec![false, true, false],
-      vec![false, false, false],
-    ];
-
-    assert_eq!(count_live_neighbours(&g, 1, 1), 0);
-  }
-  {
-    // 2x2 universe
-    let g = vec![
-      vec![false, false, false, false],
-      vec![false, true , false, false],
-      vec![false, false, true , false],
-      vec![false, false, false, false],
-    ];
-
-    assert_eq!(count_live_neighbours(&g, 1, 1), 1);
-    assert_eq!(count_live_neighbours(&g, 1, 2), 2);
-    assert_eq!(count_live_neighbours(&g, 2, 1), 2);
-    assert_eq!(count_live_neighbours(&g, 2, 2), 1);
-  }
-  {
-    // 2x3 universe
-    let g = vec![
-        //   0      1      2      3      4
-      vec![false, false, false, false, false], // 0
-      vec![false, true , false, true , false], // 1
-      vec![false, false, true , false, false], // 2
-      vec![false, false, false, false, false], // 3
-    ];
-
-    assert_eq!(count_live_neighbours(&g, 1, 1), 1);
-    assert_eq!(count_live_neighbours(&g, 1, 2), 3);
-    assert_eq!(count_live_neighbours(&g, 1, 3), 1);
-    assert_eq!(count_live_neighbours(&g, 2, 1), 2);
-    assert_eq!(count_live_neighbours(&g, 2, 2), 2);
-    assert_eq!(count_live_neighbours(&g, 2, 3), 2);
-  }
-}
-
-/* --------------------------------------------------------------------------------------------- */
-
-#[test]
-fn test_tick_cell() {
-  let g = vec![
-      //   0      1      2      3      4      5      6
-    vec![false, false, false, false, false, false, false], // 0
-    vec![false, true , false, true , false, false, false], // 1
-    vec![false, false, false, true , false, false, false], // 2
-    vec![false, false, false, true , false, false, false], // 3
-    vec![false, false, false, false, true , true , false], // 4
-    vec![false, false, false, false, true , true , false], // 5
-    vec![false, false, false, false, false, false, false], // 6
-  ];
+fn test_tick() {
+  let g = Grid::new_from(vec![
+      //   0      1      2      3      4  
+    vec![true , false, true , false, false], // 0
+    vec![false, false, true , false, false], // 1
+    vec![false, false, true , false, false], // 2
+    vec![false, false, false, true , true ], // 3
+    vec![false, false, false, true , true ], // 4
+  ]);
 
   // Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
-  assert!(!tick_cell(&g, 1, 1));
-  assert!(!tick_cell(&g, 1, 3));
+  assert!(!tick_cell(&g, 0, 0));
+  assert!(!tick_cell(&g, 0, 2));
   // Any live cell with two or three live neighbours lives on to the next generation.
-  assert!( tick_cell(&g, 2, 3));
-  assert!( tick_cell(&g, 5, 5));
+  assert!( tick_cell(&g, 1, 2));
+  assert!( tick_cell(&g, 4, 4));
   // Any live cell with more than three live neighbours dies, as if by overpopulation.
-  assert!(!tick_cell(&g, 4, 4));
+  assert!(!tick_cell(&g, 3, 3));
 
   // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-  assert!( tick_cell(&g, 4, 3));
+  assert!( tick_cell(&g, 3, 2));
   // Other dead cells.
-  assert!(!tick_cell(&g, 5, 1));
+  assert!(!tick_cell(&g, 4, 0));
+
+  let u = Universe::new_from(g);
+  assert_eq!(u.live_cells, 8);
+  assert_eq!(u.generation, 0);
+
+  let v = u.tick();
+  assert_eq!(v.live_cells, 8); // 8 -3 (dying) +3 (spawning)
+  assert_eq!(v.generation, 1);
 }
